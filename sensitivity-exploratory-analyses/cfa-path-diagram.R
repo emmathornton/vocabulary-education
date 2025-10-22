3c2d9cfdb6229844432135c41d4851f28339b13e
emmathornton <emma.thornton@manchester.ac.uk>
2025-10-22 13:31:39 +0100

    Update to latest imputed dataset and add code to check modification indices

--- sensitivity-exploratory-analyses/cfa-path-diagram.R
+++ sensitivity-exploratory-analyses/cfa-path-diagram.R
@@ -2,11 +2,15 @@
 library(mice)
 library(semTools)
 library(lavaan)
-library(semPlot)
+#library(semPlot)
 library(lavaanPlot)
 library(ggplot2)
+library(lavaan.mi)
 
 #factor analysis model on imputed dataset
+#load imputed dataset
+load("~/Documents/updated MCS datasets/education-datasetsDec22/2023-01-20_vocabulary_education_imputedMAIN/2023-01-20_vocabulary_education_imputedMAIN.Rdata")
+imputed_mcs2 = mi.res
 #specify model 
 SEP_model <- 'SEP =~ highest_nvq + oecd_income + wealth_quintiles + occupational_status + imd'
 
@@ -28,18 +32,36 @@ sepCFA.mi = cfa.mi(
 
 summary(sepCFA.mi, fit.measures = TRUE)
 
+#modification indices
+modificationindices.mi(sepCFA.mi, sort = TRUE)
+
+sep_revised = 'SEP =~ highest_nvq + oecd_income + wealth_quintiles + occupational_status + imd
+highest_nvq ~~ occupational_status'
+
+sepCFA.mi_2 = cfa.mi(
+  sep_revised, 
+  data = imputed_mcs2,
+  ordered = c("highest_nvq", "oecd_income","wealth_quintiles", "occupational_status", "imd"), 
+  std.lv=TRUE, 
+  estimator="WLSMV")
+
+summary(sepCFA.mi_2, fit.measures = TRUE)
+
+
 #SEM path diagram. 
 #substitute parameter estimates from MI model into a semPlotModel object, created from a lavaan object
 
 
 # Create a 'dummy' object, with the same model structure as we'll use in the MI method 
+#create single imputed dataset for this
+imputed_mcs2_1 <- complete(imputed_mcs2,1)
 SEP1 <- cfa(SEP_model, imputed_mcs2_1, 
             ordered = c("highest_nvq", "oecd_income","wealth_quintiles", "occupational_status", "imd"), 
             std.lv=TRUE, 
             estimator="WLSMV")
 
 # Extract the direct results from the SEM with MI data 
-desired_output <- data.frame(standardizedsolution(sepCFA.mi))
+desired_output <- data.frame(standardizedsolution(sepCFA.mi_2))
 
 
 
@@ -57,6 +79,6 @@ lavaanPlot(model = SEP1, labels=labels,
            edge_options = list(color = "grey"), coefs = T, stars="latent")
 
 
-#with semPaths: 
+
 
 
