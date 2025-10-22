e13d8a2e4e1fd804994519e770281e7329a393cd
emmathornton <emma.thornton@manchester.ac.uk>
2025-10-22 13:27:55 +0100

    save full cohort sample to compare descriptives to analytical sample

--- compile-education-data.R
+++ compile-education-data.R
@@ -2547,4 +2547,4 @@ write.csv(descriptives_analysis, file = "education_dataForContDescriptives1.csv"
 
 
 #save full cohort sample as a csv file- to get descriptives
-write.csv(descriptives_data, file = "cohort_data1.csv")
+write.csv(analysis_data, file = "cohort_data1.csv")
